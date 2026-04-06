b1f4247ff5d9595c0621fb14926522c2b657c0b5
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-07 00:52:45 +0530

    Table

---
+++ Loops Basic/TableofNineteen.java
@@ -0,0 +1,13 @@
+public class TableofNineteen {
+    public static void main(String[] args) {
+    for(int i=1;i<=10;i++){
+        System.out.println(i*19);
+    }
+    for(int i=19;i<=190;i++){
+        if (i%19==0) {
+            System.out.println(i);
+            
+        }
+    }
+    }
+}
