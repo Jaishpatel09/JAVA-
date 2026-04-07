70263fcb8ce8c5efad95080a08e7b6cdd7348e71
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-08 00:32:32 +0530

    AP

---
+++ Loops Basic/Ap.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Ap {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=2*n-1;i+=2)
+            System.out.print(i+" ");
+    }
+}
