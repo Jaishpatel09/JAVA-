57b70c32eff12110b033ea03ff917a955c7636b6
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-09 00:44:49 +0530

    Hf

---
+++ Loops Basic/HFsecMethod.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HFsecMethod {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a num: ");
+        int n=sc.nextInt();
+        int hf=1;
+        for (int i = n-1; i >=1; i--) {
+            if (n%i==0) {
+                hf=i;
+                break;
+            }
+            
+        }
+         System.out.println("Highest factor is: "+hf);   
+    }
+}
