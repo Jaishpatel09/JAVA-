9b3ed7830520c32927efdc419fb142136962beb5
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-10 22:46:13 +0530

    Prime Num

---
+++ Loops Basic/Prime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int n=sc.nextInt();
+        for (int i = 2; i < n-1; i++) {
+            if (n%i==0) {
+                System.out.println("Composite Number");
+                break;
+            }else{
+                System.out.println("Prim Number");
+                
+            }
+            
+        }
+    }
+}
