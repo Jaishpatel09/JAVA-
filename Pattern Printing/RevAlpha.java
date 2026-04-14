c30fe985a45e5cdb49b917d58a1e53a4e4422af8
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-15 00:00:44 +0530

    Trri

---
+++ Pattern Printing/RevAlpha.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class RevAlpha {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n=sc.nextInt();
+        for (int i = n; i >=1; i--) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print((char)(j+64)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
