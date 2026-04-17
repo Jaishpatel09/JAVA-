676e2a1c7f45cd2d8d901ed980636367a23f7362
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-17 10:40:40 +0530

    Rectangle Pattern

--- Pattern Printing/Rectangle.java
+++ Pattern Printing/Rectangle.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
 
-public class StarPlus {
+public class Rectangle {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.print("Enter n: ");
+        System.out.print("Enter Column: ");
         int n=sc.nextInt();
-        int mid=n/2+1;
-        for (int i = 1; i <=n; i++) {
+        System.out.print("Enter Row: ");
+        int m=sc.nextInt();
+        for (int i = 1; i <=m; i++) {
             for (int j = 1; j <=n; j++) {
-                if (i==mid || j==mid) {
+                if (i==1 || i==m || j==1 || j==n) {
                     
                     System.out.print("* ");
                 }else{
