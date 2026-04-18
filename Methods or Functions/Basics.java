1f7f44be1299b4cbefa6a64d80f1ca62e6b9b729
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-19 00:57:41 +0530

    Basic Function

---
+++ Methods or Functions/Basics.java
@@ -0,0 +1,8 @@
+public class Basics {
+    public static void fun() {
+        System.out.println("Hii...! Good Morning");
+    }
+    public static void main(String[] args) {
+        fun();
+    }
+}
