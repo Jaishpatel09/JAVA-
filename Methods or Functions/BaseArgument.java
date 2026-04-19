384caa4b869ac0291abf686889e50c37a613b8ff
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-20 00:13:54 +0530

    Argument Function

---
+++ Methods or Functions/BaseArgument.java
@@ -0,0 +1,8 @@
+public class BaseArgument {
+    public static void greet(String name) {
+        System.out.println("Good Morning... "+name);
+    }
+    public static void main(String[] args) {
+        greet("Jaish");
+    }
+}
