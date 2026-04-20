99ea4b334a4e1c1a1485d8eb450bff9a4253f56d
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-20 23:51:05 +0530

    IntroFn

---
+++ Methods or Functions/StringFn.java
@@ -0,0 +1,9 @@
+public class StringFn {
+    public static void intro(String name,int age) {
+        System.out.println("Hello ! "+name+"Your age is "+age);
+    }
+    public static void main(String[] args) {
+        intro("Jaish ", 19);
+        intro("Anshul", 20);
+    }
+}
