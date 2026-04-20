58fdffbc4c68f991ca9004db379571403348d30e
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-20 23:46:02 +0530

    Sum

---
+++ Methods or Functions/SumFn.java
@@ -0,0 +1,8 @@
+public class SumFn {
+    public static void sum(int a, int b) {
+        System.out.println("Sum is : "+(a+b));
+    }
+    public static void main(String[] args) {
+        sum(5, 7);
+    }
+}
