95a8045a88cab31221fd3f569e8a43d362f4d927
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-21 00:48:40 +0530

    Return Type

---
+++ Methods or Functions/ReturnKeyword.java
@@ -0,0 +1,11 @@
+public class ReturnKeyword {
+    public static void  greet(int a) {
+       if (a<5) {
+        System.out.println("Hii..");
+       }else
+        System.out.println("Hello");
+    }
+    public static void main(String[] args) {
+        greet(5);
+    }
+}
