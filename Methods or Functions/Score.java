b887bd3c7d022785d43e673e6c449d69df7df4f5
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-21 00:01:54 +0530

    Student

---
+++ Methods or Functions/Score.java
@@ -0,0 +1,8 @@
+public class Score {
+    public static void student(String name,int age,int classs,String grade) {
+        System.out.println("Student Name: "+name+"Age: "+age+" Class: "+classs+" Grade: "+grade);
+    }
+    public static void main(String[] args) {
+        student("Abhilash ", 16, 10,"A+");
+    }
+}
