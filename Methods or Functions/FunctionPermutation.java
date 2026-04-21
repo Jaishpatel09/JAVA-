4e777ce0c58b86fe4e55fb581ba228fce1b4a626
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-22 00:41:34 +0530

    Purmutation

---
+++ Methods or Functions/FunctionPermutation.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class FunctionPermutation {
+    public static int fact(int x){
+        int xFact=1;
+        for (int i = 1; i <=x; i++) {
+            xFact*=i;
+        }
+        return xFact;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int r=sc.nextInt();
+        
+        int npr=fact(n)/(fact(n-r));
+        System.out.println(npr);
+
+    }
+}
