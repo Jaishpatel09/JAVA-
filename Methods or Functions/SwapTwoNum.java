1e0312cdb0a1b5bccb41efd1d9e7930ee18a7eb8
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-23 00:17:07 +0530

    Swap two num

---
+++ Methods or Functions/SwapTwoNum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SwapTwoNum {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println(a+" "+b);
+        
+        int temp=a;
+            a=b;
+            b=temp;
+
+        System.out.println(a+" "+b);
+    }
+}
