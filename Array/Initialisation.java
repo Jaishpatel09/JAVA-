6652d055a2ce8157a1b6927a98a520cc7e6af306
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-25 01:27:47 +0530

    Initialisation

--- Array/Initialisation.java
+++ Array/Initialisation.java
@@ -1,6 +1,6 @@
 package Array;
 
-public class Initialization {
+public class Initialisation {
     public static void main(String[] args) {
     int[] arr=new int[6]; // Array arr is created and it have 6 elements
     System.out.println(arr[3]);
---
+++ Array/PrintArray.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr={7,9,12,15,26,52,47,89};
+        System.out.println("Size : "+arr.length);// To print size of array
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
