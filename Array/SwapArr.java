402d0f1463c42c016cd3f518cef44e490da7d3b2
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-04-28 21:38:52 +0530

    Swap Array using Pointer

---
+++ Array/SwapArr.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class SwapArr {
+    public static void print(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr={2,6,10,24,52,12,36,78,90};
+        print(arr);
+        int n=arr.length;
+        int i=0; int j=n-1;
+        while (i<j) {
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+        print(arr);
+    }
+}
