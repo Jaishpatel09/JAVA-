f44a8cb8ab0fe6230f7eb2f5a84d48e397cc1052
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-01 23:08:23 +0530

    For Each loop

---
+++ Array/ForEach.java
@@ -0,0 +1,10 @@
+package Array;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int[] arr={4,6,8,2,12,10};
+        for(int array : arr){
+            System.out.print(array+" ");
+        }
+    }
+}
