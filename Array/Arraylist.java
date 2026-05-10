9d33734eb260b1a9165e89128e3aa22b270525fd
Jaishpatel09 <jaishpatel.ktr@gmail.com>
2026-05-11 00:54:45 +0530

    Array List

---
+++ Array/Arraylist.java
@@ -0,0 +1,15 @@
+package Array;
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr= new ArrayList<>();
+        arr.add(12);
+        arr.add(32);
+        arr.add(63);
+        arr.add(100);
+        arr.add(98);
+
+        System.out.println(arr.get(2));
+        System.out.println(arr);
+    }
+}
